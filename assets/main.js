6662841aab74cd4b9281252b494d2b5e43ffc090
R3LYK <kyler.pettitt@gmail.com>
2022-07-05 16:42:16 -0500

    Added landscape view functionality to entire site. Cleaned up some CSS, polished site overall.

--- assets/main.js
+++ assets/main.js
@@ -32,25 +32,29 @@ navSlide.addEventListener('click', () => {
     const position = primaryNav.getAttribute('slide-down');
     if (position === 'false') {
         primaryNav.style.transition = '300ms ease-out';
-        primaryNav.style.padding = 'min(60vh, 45rem) 5rem';
+        primaryNav.style.paddingTop = '10vh';
         primaryNav.setAttribute('slide-down', true);
         navSlide.style.backgroundImage = "url('/assets/images/arrowUp.png')";
     } else if (position === 'true') {
         primaryNav.style.transition = '300ms ease-out';
-        primaryNav.style.padding = 'min(25vh, 12rem) 5rem';
+        primaryNav.style.paddingTop = '30vh';
         primaryNav.setAttribute('slide-down', false);
         navSlide.style.backgroundImage = "url('/assets/images/arrowDown.png')";
+        if (window.innerHeight < 450) {
+            primaryNav.style.paddingTop = '30vh';
+        } else {
+            primaryNav.style.paddingTop = '60vh';
+        }
     }
     
     console.log('position = ' + position)
 });
 
-
 function sendEmail(){
     Email.send({
         SecureToken : "4ff05753-1662-4adc-94cc-da0393527169",
-        To : 'therelyk@protonmail.com',
-        From : "pythonprojectone@gmail.com",
+        To : '<placeholder@email.com>',
+        From : "<placeholder@email.com>",
         Subject : "This is the subject",
         Body : "Name: " + document.getElementById("name").value
             + "<br> Email: " + document.getElementById("email").value
