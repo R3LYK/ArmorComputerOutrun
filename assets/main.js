5fde80645b587886fbb28bfcd8ecea737096e452
R3LYK <kyler.pettitt@gmail.com>
2022-07-18 14:02:38 -0500

    Compressed images, fixed form submit.

--- assets/main.js
+++ assets/main.js
@@ -8,6 +8,7 @@ const primaryNav = document.querySelector('.primary-navigation');
 const navToggle = document.querySelector('.mobile-nav-toggle');
 const navSlide = document.querySelector('.mobile-nav-slide');
 
+document.getElementById("greeting").innerHTML = "Bonjour";
 
 
 // ----- toggle nav on mobile ----- //
@@ -19,40 +20,14 @@ navToggle.addEventListener('click', () => {
         
         primaryNav.setAttribute('data-visible', true);
         navToggle.setAttribute('aria-expanded', true);
-        navSlide.setAttribute('aria-expanded', true);
         navSlide.style.display = 'block';
     
     } else if (visibility === 'true') {
         
         primaryNav.setAttribute('data-visible', false);
         navToggle.setAttribute('aria-expanded', false);
-        navSlide.setAttribute('aria-expanded', false);
         navSlide.style.display = 'none';
     }
 
     console.log("visibility = " + visibility)
 });
-
-// ----- menu slide on mobile ----- //
-
-// navSlide.addEventListener('click', () => {
-//     const position = primaryNav.getAttribute('slide-down');
-//     if (position === 'false') {
-//         primaryNav.style.transition = '500ms ease-in-out';
-//         primaryNav.style.paddingTop = '10vh';
-//         primaryNav.setAttribute('slide-down', true);
-//         navSlide.style.backgroundImage = "url('/assets/images/arrowDown.png')";
-//     } else if (position === 'true') {
-//         primaryNav.style.transition = '500ms ease-in-out';
-//         primaryNav.style.paddingTop = '30vh';
-//         primaryNav.setAttribute('slide-down', false);
-//         navSlide.style.backgroundImage = "url('/assets/images/arrowUp.png')";
-//         if (window.innerHeight < 450) {
-//             primaryNav.style.paddingTop = '30vh';
-//         } else {
-//             primaryNav.style.paddingTop = '60vh';
-//         }
-//     }
-    
-//     console.log('position = ' + position)
-// });
